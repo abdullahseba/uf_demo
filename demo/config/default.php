2286195ec180fa499d1126f94d418ca713178e09
abdullahseba <abdullah.seba@outlook.com>
2017-11-25 23:46:35 +0000

    Add config and and sign-in page

---
+++ demo/config/default.php
@@ -0,0 +1,18 @@
+<?php
+
+    /**
+     * Core configuration file for UserFrosting.  You must override/extend this in your site's configuration file.
+     *
+     * Sensitive credentials should be stored in an environment variable or your .env file.
+     * Database password: DB_PASSWORD
+     * SMTP server password: SMTP_PASSWORD
+     */
+
+    return [
+        // "Site" settings that are automatically passed to Twig
+        'site' => [
+
+            'title'     =>      'UserFrosting Demo'
+            ]
+
+    ];
