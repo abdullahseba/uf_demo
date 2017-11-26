14e5b7aaf328a5676935dd565531f9788e2c10f3
abdullahseba <abdullah.seba@outlook.com>
2017-11-26 00:00:55 +0000

    Add controller and routes

---
+++ demo/routes/routes.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * UserFrosting (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/UserFrosting
+ * @license   https://github.com/userfrosting/UserFrosting/blob/master/licenses/UserFrosting.md (MIT License)
+ */
+
+$app->group('/Demo', function () {
+
+
+    $this->post('/forgot-password', 'UserFrosting\Sprinkle\Demo\Controller\DemoController:demoMessage');
+
+    $this->post('/register', 'UserFrosting\Sprinkle\Demo\Controller\DemoController:demoMessage');
+
+    $this->post('/resend-verification', 'UserFrosting\Sprinkle\Demo\Controller\DemoController:demoMessage');
+
+    $this->post('/set-password', 'UserFrosting\Sprinkle\Demo\Controller\DemoController:demoMessage');
+
+    $this->post('/settings', 'UserFrosting\Sprinkle\Demo\Controller\DemoController:demoMessage')
+        ->add('authGuard')
+        ->setName('settings');
+
+    $this->post('/settings/profile', 'UserFrosting\Sprinkle\Demo\Controller\DemoController:demoMessage')
+        ->add('authGuard');
+});
---
+++ demo/src/Controller/DemoController.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+* Demo controller
+*
+* @author Abdullah Seba
+* @link example.comn
+*
+* @version 1.0.0
+*
+*/
+
+namespace UserFrosting\Sprinkle\Scheduler\Controller;
+
+use UserFrosting\Sprinkle\Core\Controller\SimpleController;
+use UserFrosting\Sprinkle\Scheduler\Controller\TaskController;
+use UserFrosting\Support\Exception\ForbiddenException;
+
+
+/**
+ * DemoController Class
+ */
+class DemoController extends SimpleController
+{
+    //Test Page
+    public function demoMessage($request, $response, $args)
+    {
+    $this->ci->alerts->addMessageTranslated('danger', 'Sorry, this feture has been disabled in the demo.');
+    }
+}
