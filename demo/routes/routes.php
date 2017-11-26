6390d6b873891762f08b60740c0d02fe206c0c6c
abdullahseba <abdullah.seba@outlook.com>
2017-11-26 00:23:44 +0000

    Update

--- demo/routes/routes.php
+++ demo/routes/routes.php
@@ -6,6 +6,9 @@
  * @license   https://github.com/userfrosting/UserFrosting/blob/master/licenses/UserFrosting.md (MIT License)
  */
 
+ /**
+  * Routes for account.
+  */
 $app->group('/account', function () {
 
     $this->post('/forgot-password', 'UserFrosting\Sprinkle\Demo\Controller\DemoController:demoMessage');
@@ -23,3 +26,28 @@ $app->group('/account', function () {
     $this->post('/settings/profile', 'UserFrosting\Sprinkle\Demo\Controller\DemoController:demoMessage')
         ->add('authGuard');
 });
+
+/**
+ * Routes for administrative user management.
+ */
+$app->group('/api/users', function () {
+    $this->delete('/u/{user_name}', 'UserFrosting\Sprinkle\Demo\Controller\UserController:demoMessage');
+
+    $this->get('', 'UserFrosting\Sprinkle\Demo\Controller\UserController:demoMessage');
+
+    $this->get('/u/{user_name}', 'UserFrosting\Sprinkle\Demo\Controller\UserController:demoMessage');
+
+    $this->get('/u/{user_name}/activities', 'UserFrosting\Sprinkle\Demo\Controller\UserController:demoMessage');
+
+    $this->get('/u/{user_name}/roles', 'UserFrosting\Sprinkle\Demo\Controller\UserController:demoMessage');
+
+    $this->get('/u/{user_name}/permissions', 'UserFrosting\Sprinkle\Demo\Controller\UserController:demoMessage');
+
+    $this->post('', 'UserFrosting\Sprinkle\Demo\Controller\UserController:demoMessage');
+
+    $this->post('/u/{user_name}/password-reset', 'UserFrosting\Sprinkle\Demo\Controller\UserController:demoMessage');
+
+    $this->put('/u/{user_name}', 'UserFrosting\Sprinkle\Demo\Controller\UserController:demoMessage');
+
+    $this->put('/u/{user_name}/{field}', 'UserFrosting\Sprinkle\Demo\Controller\UserController:demoMessage');
+})->add('authGuard');
